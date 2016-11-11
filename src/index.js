83fc253dbadf1d2759f79f9860ce34c009322678
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-11-11 23:24:14 +0100

    Add simple ARIA test

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ const Progress = define('sk-progress', class Progress extends Component {
     });
 
     return (
-      <div className="progress">
+      <div className="progress" tabindex="0" aria-valuenow={status} aria-valuemin="0" aria-valuemax="100" aria-live="polite">
         <style>{getStyles(elem)}</style>
         <span>{Progress.displayLabelContent(status, label)}</span>
       </div>
