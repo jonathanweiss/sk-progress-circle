5f77d2ceb7c9fa74fcf64971389dc044273a91f0
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-11-14 16:40:36 +0100

    Add more unit tests

--- test/progress.spec.js
+++ test/progress.spec.js
@@ -2,7 +2,75 @@ import { expect } from 'chai';
 import Progress from '../src/index';
 
 describe('sk-progress', () => {
-  it('should export a component', () => {
-    expect(true).to.equal(true);
+  let $target = null;
+
+  beforeEach(() => {
+    const element = document.createElement('div');
+    element.id = 'target';
+    document.body.appendChild(element);
+
+    $target = document.getElementById('target');
+  });
+
+  afterEach(() => {
+    $target = document.getElementById('target');
+    $target.parentNode.removeChild($target);
+  });
+
+  it('is defined', () => {
+    expect(Progress).not.to.equal(undefined);
+  });
+
+  it('renders an empty element', (done) => {
+    const emptyProgress = document.createElement('sk-progress');
+    emptyProgress.id = 'empty';
+    $target.appendChild(emptyProgress);
+
+    window.setTimeout(() => {
+      const $emptyProgress = $target.querySelector('#empty');
+
+      expect($emptyProgress.offsetWidth).to.be.above(0);
+      expect($emptyProgress.offsetHeight).to.be.above(0);
+      done();
+    }, 0);
+  });
+
+  it('renders an big, filled element', (done) => {
+    const bigProgress = document.createElement('sk-progress');
+    bigProgress.id = 'big';
+    bigProgress.status = 100;
+    bigProgress.size = 400;
+    bigProgress.color = 'blue';
+    $target.appendChild(bigProgress);
+
+    window.setTimeout(() => {
+      const $bigProgress = $target.querySelector('#big');
+
+      expect($bigProgress.offsetWidth).to.be.above(399);
+      expect($bigProgress.offsetHeight).to.be.above(399);
+
+      expect($bigProgress.status).to.equal(100);
+
+      done();
+    }, 0);
+  });
+
+  it('uses a custom label', (done) => {
+    const customProgress = document.createElement('sk-progress');
+    customProgress.id = 'custom';
+    customProgress.label = '%s percentage';
+    customProgress.status = 44;
+    customProgress.size = 200;
+    customProgress.labelColor = 'yellow';
+    customProgress.labelSize = '20px';
+    $target.appendChild(customProgress);
+
+    window.setTimeout(() => {
+      const $customProgress = $target.querySelector('#custom');
+
+      expect($customProgress.status).to.equal(44);
+
+      done();
+    }, 0);
   });
 });
