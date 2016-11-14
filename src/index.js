99cdd668737801aef08a672d96e8b9622c75aef0
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-11-14 10:18:36 +0100

    Sort props

--- src/index.js
+++ src/index.js
@@ -13,25 +13,25 @@ const Progress = define('sk-progress', class Progress extends Component {
 
   static get props() {
     return {
-      status: prop.number({
-        attribute: true,
-        default: 0,
-      }),
-      color: prop.string({
-        attribute: true,
-        default: 'green',
-      }),
+    /**
+     * @description Color to use for the background of the circle
+     * @memberof Progress
+     * @default '#a9a9a9'
+     * @type {string}
+     * @example @html <sk-progress backgroundColor="#a9a9a9"></ak-button>
+     * @example @js button.background-color = '#a9a9a9';
+     */
       backgroundColor: prop.string({
         attribute: true,
         default: '#a9a9a9',
       }),
+      color: prop.string({
+        attribute: true,
+        default: '#339900',
+      }),
       label: prop.string({
         attribute: true,
         default: '%s%',
-      }),
-      size: prop.number({
-        attribute: true,
-        default: 100,
       }),
       labelColor: prop.string({
         attribute: true,
@@ -40,6 +40,14 @@ const Progress = define('sk-progress', class Progress extends Component {
       labelSize: prop.number({
         attribute: true,
         default: 16,
+      }),
+      size: prop.number({
+        attribute: true,
+        default: 100,
+      }),
+      status: prop.number({
+        attribute: true,
+        default: 0,
       }),
     };
   }
