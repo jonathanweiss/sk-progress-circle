6790c67132d6c7a629bd70b71f93a2ca873c6dc4
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-11-14 09:13:16 +0100

    Add aria-role

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ const Progress = define('sk-progress', class Progress extends Component {
     });
 
     return (
-      <div className="progress" aria-valuenow={status} aria-valuemin="0" aria-valuemax="100" aria-live="polite">
+      <div className="progress" role="progressbar" aria-valuenow={status} aria-valuemin="0" aria-valuemax="100" aria-live="polite">
         <style type="text/css">{getStyles(elem)}</style>
         <span>{Progress.displayLabelContent(status, label)}</span>
       </div>
