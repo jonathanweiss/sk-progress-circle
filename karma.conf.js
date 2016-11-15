55edae651a2f4563248ef449586cfd01a9fcd1a8
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-11-15 21:16:54 +0100

    Add authentification for Saucelabs

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,20 @@
 const path = require('path');
+const fs = require('fs');
 const browsers = require('./karma.browsers.js');
-
 const webpackConfiguration = require('./webpack.config.js');
+
+const args = process.argv.slice(2);
+const isCI = args.length >= 2 && args[1] === '--ci';
+
+if (isCI && !process.env.SAUCE_USERNAME) {
+  if (!fs.existsSync('sauce.json')) {
+    console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
+    process.exit(1);
+  } else {
+    process.env.SAUCE_USERNAME = require('./sauce').username;
+    process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
+  }
+}
 
 // Remove default entry point since we're using test-main.js instead
 delete webpackConfiguration.entry;
