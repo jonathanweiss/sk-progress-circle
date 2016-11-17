bf0773d09bc18d80159f7fb239dc594c2d2b5b22
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-11-17 08:45:34 +0100

    Prepare generating the dist version

--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,49 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const dirSrc = path.resolve(__dirname, 'src');
 const dirTest = path.resolve(__dirname, 'test');
 const dirBuild = path.resolve(__dirname, 'build');
+const dirDist = path.resolve(__dirname, 'dist');
 const dirDemo = path.resolve(__dirname, 'demo');
 
-module.exports = {
-  entry: [
+const args = process.argv.slice(2);
+
+let entry;
+let output;
+let devtool;
+let plugins = [];
+
+if (args[0] === '-p') {
+  entry = [path.resolve(dirSrc, 'index.js')];
+  output = {
+    path: dirDist,
+    filename: 'all.min.js',
+  };
+} else {
+  entry = [
     path.resolve(dirSrc, 'index.js'),
     'webpack-dev-server/client?http://127.0.0.1:4000',
     'webpack/hot/only-dev-server',
-  ],
-  output: {
+  ];
+  output = {
     path: dirBuild,
     filename: 'bundle.js',
-  },
+  };
+  plugins = [
+    // Simply copies the files over
+    new CopyWebpackPlugin([
+        { from: dirDemo }, // to: output.path
+    ]),
+    // Avoid publishing files when compilation fails
+    new webpack.NoErrorsPlugin(),
+  ];
+  // Create Sourcemaps for the bundle
+  devtool = 'source-map';
+}
+
+module.exports = {
+  entry,
+  output,
+  plugins,
+  devtool,
   devServer: {
     contentBase: dirBuild,
     historyApiFallback: true,
@@ -40,19 +71,8 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    // Simply copies the files over
-    new CopyWebpackPlugin([
-        { from: dirDemo }, // to: output.path
-    ]),
-    // Avoid publishing files when compilation fails
-    new webpack.NoErrorsPlugin(),
-  ],
   stats: {
     // Nice colored output
     colors: true,
   },
-
-  // Create Sourcemaps for the bundle
-  devtool: 'source-map',
 };
