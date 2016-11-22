0e3aed8166630c806410f290749e301dce6d79f0
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-11-22 21:03:35 +0100

    chore(tooling): Correct webpack configuration and add dist file

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,6 @@ if (args[0] === '-p') {
     filename: 'all.min.js',
   };
   plugins = [
-    new webpack.optimize.CommonsChunkPlugin('common.js'),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
